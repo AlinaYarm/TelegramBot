c9e31bcce6df9d6552365613ac2caf7b46cb8b01
ayvm <alina00802@gmail.com>
2022-10-23 14:03:20 +0400

    #1 созданы таблица, записывающая пользователей

--- venv/main.py
+++ venv/main.py
@@ -22,57 +22,57 @@ companion = ['Аня', 'Павел', 'Анастасия', 'Григорий', '
 # author_name=models.CharField('Имя автора',max_length=50)
 # comment_text=models.CharField('Текст комментария',max_length=200)
 
-# conn = sqlite3.connect('share/main.py', check_same_thread=False)
-# cursor = conn.cursor()
-#
-# @bot.message_handler(content_types=['text'])
-# def get_text_messages(message):
-#     if message.text.lower() == 'привет':
-#         bot.send_message(message.chat.id, 'Привет! Ваше имя добавлено в базу данных!')
-#
-#         us_id = message.from_user.id
-#         us_name = message.from_user.first_name
-#         us_sname = message.from_user.last_name
-#         username = message.from_user.username
-#
-#         db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
+connection = sqlite3.connect('User.db', check_same_thread=False)
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS User
+              (Name TEXT, Surname TEXT, ID INT)''')
+#, User_code INT
+connection.commit()
+#connection.close()
 
-# def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
-# 	cursor.execute('INSERT INTO test (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
-# 	conn.commit()
+
+def db_table_val(Name: str, Surname: str, ID: int):
+    cursor.execute('INSERT INTO User (Name, Surname, ID) VALUES (?, ?, ?)', (Name, Surname, ID))
+    #cursor.execute(f'INSERT INTO {message.from_user.name} (Name, Surname, ID) VALUES (?, ?, ?)', (Name, Surname, ID))
+    connection.commit()
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn_add = types.KeyboardButton("Добавь меня в базу!")
     btn1 = types.KeyboardButton("👋 Поздороваться")
     btn2 = types.KeyboardButton("Начать чаепитие ☕️")
     help_btn = telebot.types.KeyboardButton("/help")
-    markup.add(btn1, btn2, help_btn)
+    markup.add(btn1, btn2,btn_add, help_btn)
     bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я бот, который рандомно выберет тебе собеседника для совместного чаепития".format(
                          message.from_user), reply_markup=markup)
 
-connection = sqlite3.connect('User.db')
-cursor = connection.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS User
-              (Name TEXT, Surname TEXT, ID INT)''')
-connection.commit()
-connection.close()
 
 @bot.message_handler(content_types=['text'])
 def func(message):
-    if (message.text == "👋 Поздороваться"):
+    if message.text == "Добавь меня в базу!":
+        bot.send_message(message.chat.id, "Привет! Ваше имя добавлено в базу данных!")
+        us_name = message.from_user.first_name
+        us_sname = message.from_user.last_name
+        us_id = message.from_user.id
+        #us_code = message.from_user.code
+        db_table_val(Name = us_name, Surname = us_sname, ID = us_id)
+        #, User_code=us_code)
+    elif message.text == "👋 Поздороваться":
         bot.send_message(message.chat.id, text="Привеет.. Спасибо что ты с нами!)")
-    elif (message.text == "Начать чаепитие ☕️"):
+    elif message.text == "Начать чаепитие ☕️":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         tea = telebot.types.KeyboardButton("Пора пить чай!")
         start = telebot.types.KeyboardButton("/start")
         markup.add(tea, start)
-        bot.send_message(message.chat.id, 'Нажми кнопку "Пора пить чай!", и я тебе отправлю имя твоего собеседника! \n А если хочешь вернуться в меню нажми "start"',
+        bot.send_message(message.chat.id, 'Нажми кнопку "Пора пить чай!", и я тебе отправлю имя твоего собеседника! '
+                                          '\n А если хочешь вернуться в меню нажми "start"',
                          reply_markup=markup)
-    elif (message.text == "Пора пить чай!"):
+    elif message.text == "Пора пить чай!":
         bot.send_message(message.chat.id, random.choice(companion))
 
-    elif (message.text == "/help"):
+    elif message.text == "/help":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("Как тебя зовут?")
         btn2 = types.KeyboardButton("Что ты можешь?")
@@ -80,13 +80,13 @@ def func(message):
         markup.add(btn1, btn2, back)
         bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
 
-    elif (message.text == "Как тебя зовут?"):
+    elif message.text == "Как тебя зовут?":
         bot.send_message(message.chat.id, "У меня нет имени..")
 
     elif message.text == "Что ты можешь?":
         bot.send_message(message.chat.id, text="Выбрать тебе собеседника ☕️")
 
-    elif (message.text == "Вернуться в главное меню"):
+    elif message.text == "Вернуться в главное меню":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton("👋 Поздороваться")
         button2 = types.KeyboardButton("Начать чаепитие ☕️")
@@ -94,7 +94,7 @@ def func(message):
         markup.add(button1, button2, button3)
         bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
     else:
-        bot.send_message(message.chat.id, text="На такую комманду я не запрограммирован..")
+        bot.send_message(message.chat.id, text="На такую команду я не запрограммирован..")
 
 
 bot.polling(none_stop=True)
